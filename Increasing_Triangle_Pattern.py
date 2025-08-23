7eb16b75fdee465dd1d49a03075326f6eda37b32
Aditya Srivastava <yeahboyadi@gmail.com>
2025-08-23 09:06:19 +0530

    Delete Increasing_Triangle_Pattern.py

--- Increasing_Triangle_Pattern.py
+++
@@ -1,9 +0,0 @@
-n = 5  # Number of rows
-
-for i in range(n):  # Outer loop runs 'n' times -> O(n)
-    for j in range(i + 1):  # Inner loop runs (i+1) times -> Total O(n^2)
-        print("*", end='  ')  # Printing each star, no extra space used except loop variables
-    print()  # Move to the next line after each row
-
-# TC: O(n^2) -> Nested loops
-# SC: O(1)  -> Constant extra space (only loop counters)
